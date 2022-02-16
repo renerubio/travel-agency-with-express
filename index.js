b110c94aebf3ea5a496154b1ce584e9ac85702f8
renerubio <renerubio.dev@hotmail.com>
2022-02-16 11:13:14 +0100

    feat: DB MySql and sequelize

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-
 import express from "express";
 import router from "./routes/index.js";
+import db from "./config/db.js";
 
 const app = express();
+
+// Conectar base de datos
+db.authenticate()
+  .then(()=> console.log('Base de datos conectada'))
+  .catch(error => console.log(error))
 
 // Definir puerto
 const port = process.env.PORT || 4000;
 
 // Habilitar pug
 app.set('view engine', 'pug');
+
+// año actual
+app.use((req, res, next)=>{
+  const year = new Date();
+
+  res.locals.actualYear = year.getFullYear();
+  res.locals.nombreSitio = 'Agencia de Viajes';
+  
+  next();
+});
 
 // Definir la carpeta pública
 app.use(express.static('public'));
